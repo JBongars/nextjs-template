784ead8343a83cf35c7b0082d68a835ac2f59f50
julien bongars <julienbongars23@gmail.com>
2023-02-20 05:55:58 +0800

    feat: github actions + fake tests

---
+++ src/__tests__/demotest.spec.js
@@ -0,0 +1,95 @@
+describe("# Home Page Component", () => {
+  describe("## Navigation Button", () => {
+    it("Should render the navigation button component without errors", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have a navigation button element", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have the correct text displayed on the navigation button", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have a function attached to the navigation button click event", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should navigate to the home page when the navigation button is pressed", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should not navigate to the home page when the user is already on the home page", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should navigate to the about page when the about navigation button is pressed", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should navigate to the contact page when the contact navigation button is pressed", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should not navigate to a non-existent page when the corresponding navigation button is pressed", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have an active class on the navigation button when the user is on that page", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have a different active class for each navigation button", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should not have an active class on any other navigation button when the user is on the home page", () => {
+      expect(true).toEqual(true);
+    });
+  });
+
+  describe("## Hero Banner", () => {
+    it("Should render the hero banner component without errors", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have a hero banner element", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have the correct text displayed on the hero banner", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have a call to action button element", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have the correct text displayed on the call to action button", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have a function attached to the call to action button click event", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should navigate to the about page when the call to action button is pressed", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should not navigate to the home page when the call to action button is pressed", () => {
+      expect(true).toEqual(true);
+    });
+  });
+
+  describe("## Featured Products Section", () => {
+    it("Should render the featured products section component without errors", () =>
+      expect(true).toEqual(true));
+    it("Should have a featured products section element", () =>
+      expect(true).toEqual(true));
+    it("Should have a list of featured products", () =>
+      expect(true).toEqual(true));
+    it("Should have the correct number of featured products displayed", () =>
+      expect(true).toEqual(true));
+    it("Should have each featured product with an image, name, description, and price", () =>
+      expect(true).toEqual(true));
+    it("Should have a function attached to each featured product click event", () =>
+      expect(true).toEqual(true));
+    it("Should navigate to the product page for that featured product when clicked", () =>
+      expect(true).toEqual(true));
+    it("Should not navigate to a non-existent product page when a non-existent featured product is clicked", () =>
+      expect(true).toEqual(true));
+  });
+
+  describe("## Footer", () => {
+    it("Should render the footer component without errors", () => {
+      expect(true).toEqual(true);
+    });
+    it("Should have a footer element with the correct text displayed", () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
